90aa7fe8d65f59433c9586cb612cb29eada71aea
Ben Chandler <benchanwaal@yahoo.co.uk>
2023-09-23 17:48:21 +0100

    Add test for bag

--- src/factories/bag.rs
+++ src/factories/bag.rs
@@ -1,7 +1,6 @@
 use crate::tiles::Tiles;
 use rand::seq::SliceRandom;
 
-
 const NUM_TILES: usize = 100;
 pub struct Bag {
     contents: [Tiles; NUM_TILES as usize],
@@ -39,5 +38,66 @@ impl Bag {
     pub fn reset_bag(&mut self) {
         self.seen = Vec::new();
         self.unseen = self.contents.to_vec();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::seq::SliceRandom;
+    use crate::factories::bag::Bag;
+    use crate::tiles::Tiles;
+
+    #[test]
+    fn test_new_bag() {
+        let bag = Bag::new();
+        let contents = &bag.contents;
+        // Ensure that the bag has the correct number of tiles
+        assert_eq!(bag.contents.len(), NUM_TILES);
+
+        // Ensure that all tiles are from the predefined options
+        let tile_options = [Tiles::Red, Tiles::DeepBlue, Tiles::IceBlue, Tiles::Black, Tiles::Yellow];
+        for tile in &bag.contents {
+            assert!(tile_options.contains(tile));
+        }
+    }
+
+    #[test]
+    fn test_get_factory_tiles() {
+        let mut bag = Bag::new();
+
+        // Ensure that calling get_factory_tiles returns 4 tiles and they are removed from unseen
+        let factory_tiles = bag.get_factory_tiles();
+        assert_eq!(factory_tiles.len(), 4);
+        assert_eq!(bag.unseen.len(), NUM_TILES - 4);
+    }
+
+    #[test]
+    fn test_is_empty() {
+        let mut bag = Bag::new();
+        assert!(!bag.is_empty()); // Bag should not be empty initially
+
+        // Drain the bag to make it empty
+        while !bag.is_empty() {
+            bag.get_factory_tiles();
+        }
+
+        assert!(bag.is_empty()); // Bag should be empty after draining
+    }
+
+    #[test]
+    fn test_reset_bag() {
+        let mut bag = Bag::new();
+
+        // Drain some tiles to simulate gameplay
+        for _ in 0..10 {
+            bag.get_factory_tiles();
+        }
+
+        // Reset the bag and ensure it's back to its initial state
+        bag.reset_bag();
+        assert_eq!(bag.seen.len(), 0);
+        assert_eq!(bag.unseen.len(), NUM_TILES);
+        assert_eq!(bag.contents.len(), NUM_TILES);
     }
 }
